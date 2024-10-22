2daa07f73b110548681e358682339af71dd7ecb3
AnusreeSreekumar <anusree.ks@gmail.com>
2024-10-22 15:06:36 +0530

    commit

--- Backend-ClassWork/Routes/adminRoutes.js
+++ Backend-ClassWork/Routes/adminRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; //this module is used for generating tokens
+import { authenticate } from "../../Middleware/auth.js";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const route = Router();
-const user = new Map(); 
-const secretKey = 'hello';
+const user = new Map();
+const secretKey = process.env.SecretKey;
+const addcourses = new Map();
 
 route.get('/', (req, res) => {
 
@@ -42,40 +46,413 @@ route.post('/signup', async (req, res) => {
     }
 })
 
-route.post('/login',async(req, res) => {
+route.post('/login', async (req, res) => {
 
-    const {Username, Password} = req.body;
-    
+    const { Username, Password } = req.body;
+
     //  console.log(Username);
-    
+
     const input = user.get(Username);
     // console.log(input);
 
-    if(user.has(Username)){
+    if (user.has(Username)) {
 
         // res.status(200).json({message : "User present"})
-        const isvalid = await bcrypt.compare(Password,input.newP)
+        const isvalid = await bcrypt.compare(Password, input.newP)
         // console.log(isvalid);
-        if(isvalid){
-            const token =  jwt.sign({username : Username, UserRole : input.Role},secretKey,{expiresIn : '1h'})
+        if (isvalid) {
+            const token = jwt.sign({ userName: Username, userRole: input.Role }, secretKey, { expiresIn: '1h' })
             console.log(token);
             res.cookie('authtoken', token, {
-                httpOnly : true
+                httpOnly: true
             });
-            res.status(200).json({message : "Success"})
-            
+            res.status(200).json({ message: "Success" })
+
             // res.status(200).json({message : "User credentials are valid"})
         }
-        else{
-            res.status(103).json({message : "Please check your credentials"})
+        else {
+            res.status(103).json({ message: "Please check your credentials" })
         }
     }
-    else{
+    else {
 
         res.status(103).json({ message: "Please register" });
         console.log("User not present. Please register");
     }
 
 })
+
+route.post('/addCourse', authenticate, (req, res) => {
+
+    const loginName = req.UserName;
+    const loginRole = req.UserRole;
+    // const addcourses = new Map();
+
+    console.log(loginRole);
+
+
+    const {
+        CourseName,
+        CourseId,
+        CourseType,
+        Description,
+        Price
+    } = req.body;
+
+    try {
+
+        // const CourseName = CourseName.trim().toLowerCase()
+
+        if (loginRole == 'admin') {
+
+            addcourses.set(CourseId, { CourseName, CourseType, Description, Price })
+            res.status(201).json({ message: "Course added" });
+            console.log(addcourses);
+            console.log("Course added successfully");
+
+        }
+        else {
+            res.status(401).json({ message: "User role should be Admin" });
+
+        }
+    }
+    catch (error) {
+
+        res.status(500).json({ message: "No response from Client" })
+    }
+})
+
+//getCourse using Post method
+
+route.post('/getCourse', authenticate, (req, res) => {
+
+    const loginName = req.UserName;
+
+    try {
+
+        if (loginName) {
+
+            const result = []
+            // const data = ;
+            const data = req.body;
+            const searchItem = data.Search;
+
+            if (searchItem) {
+                for (const [id, item] of addcourses) {
+                    if (id.includes(searchItem) || item.CourseName.includes(searchItem) || item.CourseType.includes(searchItem)) {
+                        result.push(
+                            id,
+                            item.CourseName,
+                            item.CourseType,
+                            item.Description,
+                            item.Price
+                        );
+                    }
+                }
+                if (result.length > 0) {
+
+                    res.status(200).json({
+                        message: "Course details are:",
+                        courses: result
+                    });
+                    console.log("Course details published");
+
+                    // break;
+                }
+                else {
+
+                    res.status(404).json({ message: "Search not found" });
+                    console.log("Searched course not available");
+                }
+            }
+            else {
+
+                res.status(404).json({ message: "Course not available" });
+                console.log("No such Course available");
+
+            }
+
+        }
+        else {
+
+            console.log("Please login");
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Client server missing" });
+
+    }
+});
+
+//getCourse using params in GET method
+
+route.get('/getCourse/:id', authenticate, (req, res) => {
+
+    const loginName = req.UserName;
+    try {
+        if (loginName) {
+
+            const searchItem = req.params.id;
+            const result = [];
+            if (searchItem) {
+                if (addcourses.size > 0) {
+
+                    for (const [id, item] of addcourses) {
+                        if (id.includes(searchItem) || item.CourseName.includes(searchItem) || item.CourseType.includes(searchItem)) {
+                            result.push(
+                                id,
+                                item.CourseName,
+                                item.CourseType,
+                                item.Description,
+                                item.Price
+                            );
+                        }
+                    }
+                } else {
+
+                    console.log("Map is empty");
+
+                }
+                if (result.length > 0) {
+
+                    res.status(200).json({
+                        message: "Course details are:",
+                        courses: result
+                    });
+                    console.log("Course details published");
+                }
+                else {
+
+                    res.status(404).json({ message: "Searched Course not found" });
+                }
+            }
+            else {
+
+                console.log("No such Course available");
+
+            }
+        }
+        else {
+
+            console.log("Please login");
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Client server missing" });
+
+    }
+});
+
+//getCourse using query in GET method
+
+route.get('/getCourse', (req, res) => {
+
+    try {
+
+        const searchItem = req.query.CourseName;
+        const result = [];
+        if (searchItem) {
+            if (addcourses.size > 0) {
+
+                for (const [id, item] of addcourses) {
+                    if (id.includes(searchItem) || item.CourseName.includes(searchItem) || item.CourseType.includes(searchItem)) {
+                        result.push(
+                            id,
+                            item.CourseName,
+                            item.CourseType,
+                            item.Description,
+                            item.Price
+                        );
+                    }
+                }
+            }
+            else {
+
+                console.log("Map is empty");
+
+            }
+            if (result.length > 0) {
+
+                res.status(200).json({
+                    message: "Course details are:",
+                    courses: result
+                });
+                console.log("Course details published");
+            }
+            else {
+
+                res.status(404).json({ message: "Searched Item is not matching" });
+            }
+        }
+        else {
+
+            res.status(404).json({ message: "No such course available" });
+
+        }
+    }
+    catch (error) {
+
+        res.status(500).json({ message: "Client server missing" });
+    }
+
+});
+
+//updateCourse using POST request
+
+route.post('/updateCourse', authenticate, (req, res) => {
+
+    const loginRole = req.UserRole;
+
+    try {
+
+        if (loginRole == 'admin') {
+
+            const data = req.body;
+            const updateId = data.CourseId;
+            console.log("Inside first If", data);
+
+            if (addcourses.has(updateId)) {
+
+                const mapitem = addcourses.get(updateId);
+                // console.log("Inside 2nd If",mapitem);
+                const {
+                    CourseId,
+                    newCourseName,
+                    newCourseType,
+                    newDescription,
+                    newPrice
+                } = data;
+
+                mapitem.CourseId = CourseId || mapitem.CourseId;
+                mapitem.CourseName = newCourseName || mapitem.CourseName;
+                mapitem.CourseType = newCourseType || mapitem.CourseType;
+                mapitem.Description = newDescription || mapitem.Description;
+                mapitem.Price = newPrice || mapitem.Price;
+                addcourses.set(CourseId, mapitem);
+                console.log("after update ", mapitem);
+            }
+        }
+    }
+    catch (error) {
+
+        res.status(500).json({ message: "Client server missing" });
+    }
+
+});
+
+//updateCourse using PUT request
+
+route.put('/updateCourse/:id', authenticate, (req, res) => {
+
+    const loginRole = req.UserRole;
+
+    try {
+
+        if (loginRole == 'admin') {
+
+            const data = req.body;
+            const updateId = req.params.id;
+
+            if (addcourses.has(updateId)) {
+
+                const mapitem = addcourses.get(updateId);
+                const {
+                    newCourseName,
+                    newCourseType,
+                    newDescription,
+                    newPrice
+                } = data;
+
+                mapitem.CourseName = newCourseName;
+                mapitem.CourseType = newCourseType;
+                mapitem.Description = newDescription;
+                mapitem.Price = newPrice;
+                addcourses.set(updateId, mapitem);
+                res.status(200).json({ message: "Course updated successfully" });
+                console.log(addcourses);
+            }
+        }
+    }
+    catch (error) {
+
+        res.status(500).json({ message: "Client server missing" });
+    }
+
+});
+
+//updateCourse using PATCH request
+
+route.patch('/updateCourse/:id', authenticate, (req, res) => {
+
+    const loginRole = req.UserRole;
+
+    try {
+
+        if (loginRole == 'admin') {
+
+            const data = req.body;
+            const updateId = req.params.id;
+            // console.log("Inside first if ID: ",updateId);
+
+
+            if (addcourses.has(updateId)) {
+
+                const mapitem = addcourses.get(updateId);
+                // console.log("Inside 2nd if Map: ",mapitem);
+                const {
+                    newCourseName,
+                    newCourseType,
+                    newDescription,
+                    newPrice
+                } = data;
+
+                mapitem.CourseName = newCourseName || mapitem.CourseName;
+                mapitem.CourseType = newCourseType || mapitem.CourseType;
+                mapitem.Description = newDescription || mapitem.Description;
+                mapitem.Price = newPrice || mapitem.Price;
+                addcourses.set(updateId, mapitem);
+                res.status(200).json({ message: "Course updated successfully" });
+                console.log(addcourses);
+            }
+            else {
+
+            }
+        }
+    }
+    catch (error) {
+
+        res.status(500).json({ message: "Client server missing" });
+    }
+});
+
+route.delete('/deleteCourse/:id', authenticate, (req, res) => {
+
+    const loginRole = req.UserRole;
+    try {
+
+        if (loginRole == 'admin') {
+
+            const data = req.params.id;
+            // const updateId = 
+            if (addcourses.has(data)) {
+
+                addcourses.delete(data);
+                res.status(200).json({ message: "Course Deleted" });
+                console.log("Course Deleted");
+            }
+            else {
+
+                res.status(404).json({ message: "No Course Found" });
+            }
+        }
+        else {
+            console.log("Please login as Admin");
+
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Client Server missing" });
+    }
+});
 
 export { route };
--- Backend-ClassWork/index.js
+++ Backend-ClassWork/index.js
@@ -1,12 +1,14 @@
 import express, { json } from 'express';
 
 import { route } from './Routes/adminRoutes.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express(); //creating an instance of imported 'express'
 app.use(json()) //we call inbuilt functions using express instance(app).use
 app.use('/',route)
 
-const port = 8000;
+const port = process.env.port;
 
 app.listen(port, () => {
     console.log(`Server is listening to port: ${port}`)
